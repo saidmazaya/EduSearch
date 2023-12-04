d387a305a3d764743aff416083c6ee6706ef20a9
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-12-04 15:27:38 +0700

    Add haversine formula and terdekat route

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from rdflib.plugins.sparql import prepareQuery
 from SPARQLWrapper import SPARQLWrapper, JSON
 from flask import render_template, request, url_for
 import json
+import math
 
 app = Flask(__name__)
 # Deklarasi Namespace
@@ -184,6 +185,75 @@ def detail(npsn):
     # Pass the results to the template
     return render_template('detail.html', npsn=npsn, data=data_detail)
 
+# Haversine formula to calculate distance between two sets of coordinates
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371000  # Radius of the Earth in meters
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
+
+@app.route('/terdekat', methods=['GET', 'POST'])
+def terdekat():
+    # SPARQL query to retrieve details for the specified NPSN
+    query_terdekat = f"""
+    {PREFIXES}
+
+    SELECT DISTINCT  ?namaSekolah ?npsn ?akreditasi ?bentukPendidikan ?kecamatan ?lintang ?bujur
+    WHERE {{
+        OPTIONAL {{
+            ?individu instansi:namaSekolah ?namaSekolah ;
+                    instansi:NPSN ?npsn ;
+                    instansi:lintang ?lintang ;
+                    instansi:bujur ?bujur ;
+                    instansi:akreditasi ?akreditasi ;
+                    instansi:kindOf ?bentukPendidikan ;
+                    instansi:locatedIn ?kecamatan .
+        }}
+    }}
+    """
+
+    user_latitude_str = request.args.get('latitude', '0')
+    user_longitude_str = request.args.get('longitude', '0')
+
+    try:
+        user_latitude = float(user_latitude_str)
+        user_longitude = float(user_longitude_str)
+    except ValueError:
+        # Handle the case where parsing to float fails
+        return "Invalid latitude or longitude", 400
+    
+        # Print the values for debugging
+    print(f"User Latitude: {user_latitude}")
+    print(f"User Longitude: {user_longitude}")
+
+    # Assuming you have a function to query the SPARQL endpoint for schools
+    # Replace this with your actual SPARQL query and logic
+    data_terdekat = run_query(query_terdekat)
+
+    # Calculate distance for each school and add it to the data
+    for school in data_terdekat:
+        school_latitude_str = school["lintang"]["value"]
+        school_longitude_str = school["bujur"]["value"]
+
+        try:
+            school_latitude = float(school_latitude_str)
+            school_longitude = float(school_longitude_str)
+        except ValueError:
+            # Handle the case where parsing to float fails
+            school["distance"] = None
+            continue
+
+        distance = haversine(user_latitude, user_longitude, school_latitude, school_longitude)
+        school["distance"] = distance
+
+    # Execute the SPARQL query
+    bentuk_pendidikan_classification = get_bentuk_pendidikan_classification()
+
+    # Pass the results to the template
+    return render_template('terdekat.html', data=data_terdekat, bentuk_pendidikan_classification=bentuk_pendidikan_classification)
 
 if __name__ == '__main__':
     app.run(debug=True)
